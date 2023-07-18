e4ab181001df12f263e87dd5a700a8735523f842
anoema <anoemangi@gmail.com>
2023-07-17 23:47:01 -0300

    Adição de class de tratamente de valor de moeda e color

---
+++ ControleFinanceiroApp/Libraries/Converters/TransactionValueColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using ControleFinanceiroApp.Model;
+using System.Globalization;
+
+namespace ControleFinanceiroApp.Libraries.Converters;
+
+public class TransactionValueColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        Transaction transaction = value as Transaction;
+        if (transaction == null)
+        {
+            return Colors.Black;
+        }
+        return
+            transaction.Type == TransactionType.Income ? Color.FromRgba("#FF939E5A") : Colors.Red;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ControleFinanceiroApp/Libraries/Converters/TransactionValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using ControleFinanceiroApp.Model;
+using System.Globalization;
+
+namespace ControleFinanceiroApp.Libraries.Converters;
+
+public class TransactionValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        Transaction transaction = value as Transaction;
+        if (transaction == null)
+        {
+            return string.Empty;
+        }
+        return
+            transaction.Type == TransactionType.Income ? transaction.Value.ToString("C") :
+            $"-{transaction.Value.ToString("C")}";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
